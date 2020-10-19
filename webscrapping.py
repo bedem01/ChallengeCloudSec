229928464ce4345094a8003dc49dc864cd0f43c6
Bedem <50812645+bedem01@users.noreply.github.com>
2020-10-19 08:07:01 -0300

    Update webscrapping.py

--- webscrapping.py
+++ webscrapping.py
@@ -1,12 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-result = requests.get("https://www.dan.me.uk/tornodes/")
-#print(result.status_code)
-#print(result.headers)
+
+result = requests.get("https://www.dan.me.uk/torlist/")
+
 src = result.content
-#print (src)
-soup = BeautifulSoup(src, 'lxml')
-print(soup)
-links = soup.find_all('__BEGIN_TOR_NODE_LIST__')
-print(links)
-#print("\n")
+
+soup = BeautifulSoup(src, "lxml")
+
+print(soup.p.text)
+
